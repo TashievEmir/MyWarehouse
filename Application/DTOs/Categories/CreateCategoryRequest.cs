59068f64033a7205467f7d11df11785c1927b65e
Emir Tashiev <tashievemir01@gmail.com>
2026-02-09 16:10:38 +0600

    added models to work with category service

---
+++ Application/DTOs/Categories/CreateCategoryRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs.Categories
+{
+    public class CreateCategoryRequest
+    {
+        public string Name { get; set; } = "";
+        public string? Description { get; set; }
+    }
+}
---
+++ Application/DTOs/Categories/UpdateCategoryRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs.Categories
+{
+    public class UpdateCategoryRequest
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = "";
+        public string? Description { get; set; }
+    }
+}
