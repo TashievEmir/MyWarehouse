34273ad73fc75056abe7c0d5ede3f6a5b6c9a322
Emir Tashiev <tashievemir01@gmail.com>
2026-02-09 15:45:29 +0600

    added models to work with inventory service

---
+++ Application/DTOs/Inventories/AdjustInventoryRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs.Inventories
+{
+    public class AdjustInventoryRequest
+    {
+        public long ProductId { get; set; }
+        public int Delta { get; set; } // +increase / -decrease
+    }
+}
