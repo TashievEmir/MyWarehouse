b7c3da33bf4a88937e9758bc6e3c2408ec37231c
Emir Tashiev <tashievemir01@gmail.com>
2026-02-09 16:12:01 +0600

    added category service

---
+++ Application/Contracts/Interfaces/ICategoryService.cs
@@ -0,0 +1,20 @@
+﻿using Application.DTOs.Categories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Contracts.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<long> CreateAsync(CreateCategoryRequest request, CancellationToken ct);
+
+        Task UpdateAsync(UpdateCategoryRequest request, CancellationToken ct);
+
+        Task DeleteAsync(long id, CancellationToken ct);
+
+        Task<CategoryResponse?> GetAsync(long id, CancellationToken ct);
+
+        Task<List<CategoryResponse>> GetAllAsync(CancellationToken ct);
+    }
+}
