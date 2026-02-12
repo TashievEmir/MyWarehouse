a7309289c0efed75695dabfccb761822608c99a2
Emir Tashiev <tashievemir01@gmail.com>
2026-02-12 14:10:31 +0600

    changed the user entity by ddd principle

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,14 +7,36 @@ namespace Domain.Entities
 {
     public class User
     {
-        public long Id { get; set; }
-        public string Username { get; set; } = null!;
-        public string PasswordHash { get; set; } = null!;
-        public string FullName { get; set; } = null!;
-        public string Role { get; set; } = "cashier";
-        public DateTimeOffset CreatedAt { get; set; }
+        public long Id { get; private set; }
 
-        public ICollection<Sale> Sales { get; set; } = new List<Sale>();
-        public ICollection<DebtPayment> DebtPayments { get; set; } = new List<DebtPayment>();
+        public string Username { get; private set; }
+        public string PasswordHash { get; private set; }
+        public string FullName { get; private set; }
+
+        public string Role { get; private set; }
+
+        public DateTimeOffset CreatedAt { get; private set; }
+
+        public ICollection<Sale> Sales { get; private set; } = new List<Sale>();
+        public ICollection<DebtPayment> DebtPayments { get; private set; } = new List<DebtPayment>();
+
+        private User() { } // EF ctor
+
+        public User(string username, string password, string fullName, string role)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+                throw new DomainException("Username required");
+
+            Username = username;
+            PasswordHash = BCrypt.Net.BCrypt.HashPassword(password);
+            FullName = fullName;
+            Role = role;
+            CreatedAt = DateTimeOffset.UtcNow;
+        }
+
+        public bool VerifyPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, PasswordHash);
+        }
     }
 }
