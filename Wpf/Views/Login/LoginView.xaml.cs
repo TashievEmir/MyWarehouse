25b1e237662e469b3bb0c199b8c25e71157db7df
Emir Tashiev <tashievemir01@gmail.com>
2026-02-16 21:49:03 +0600

    add enabling see the password

--- Wpf/Views/Login/LoginView.xaml.cs
+++ Wpf/Views/Login/LoginView.xaml.cs
@@ -4,8 +4,29 @@ namespace Wpf.Views.Login;
 
 public partial class LoginView : Window
 {
+    private bool _showPassword = false;
+    
     public LoginView()
     {
         InitializeComponent();
     }
+    
+    private void TogglePassword(object sender, RoutedEventArgs e)
+    {
+        if (_showPassword)
+        {
+            PasswordBox.Password = VisiblePassword.Text;
+            PasswordBox.Visibility = Visibility.Visible;
+            VisiblePassword.Visibility = Visibility.Collapsed;
+        }
+        else
+        {
+            VisiblePassword.Text = PasswordBox.Password;
+            VisiblePassword.Visibility = Visibility.Visible;
+            PasswordBox.Visibility = Visibility.Collapsed;
+        }
+
+        _showPassword = !_showPassword;
+    }
+    
 }
