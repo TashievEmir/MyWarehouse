cd79b82bd5ddf436e7060b2b157e8840a658750d
Emir Tashiev <tashievemir01@gmail.com>
2026-03-04 12:39:31 +0600

    made sidebar moving

--- Wpf/MainWindow.xaml.cs
+++ Wpf/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Wpf.Common;
 using Wpf.ViewModels;
 
 namespace Wpf
@@ -17,10 +18,29 @@ namespace Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool _sidebarOpened = true;
+        
         public MainWindow(MainViewModel vm)
         {
             InitializeComponent();
             DataContext = vm;
+        }
+        
+        private void ToggleSidebar(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            double from = SidebarColumn.Width.Value;
+            double to = _sidebarOpened ? 60 : 220; // закрытый / открытый размер
+
+            var animation = new GridLengthAnimation
+            {
+                From = new GridLength(from),
+                To = new GridLength(to),
+                Duration = new Duration(TimeSpan.FromMilliseconds(200))
+            };
+
+            SidebarColumn.BeginAnimation(ColumnDefinition.WidthProperty, animation);
+
+            _sidebarOpened = !_sidebarOpened;
         }
     }
 }
